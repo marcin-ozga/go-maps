5c0027ef745bff2cbc5fd20ed8789512a9618b4f
Marcin Ozga <nuklearny45@poczta.onet.pl>
2024-06-04 21:54:19 +0200

    Any, All

--- Where.go
+++ Where.go
@@ -9,3 +9,21 @@ func Where[Dictionary map[Key]Value, Key comparable, Value any](dictionary Dicti
 	}
 	return m
 }
+
+func Any[Key comparable, Value any](dictionary map[Key]Value, keyValueSelector func(key Key, value Value) bool) bool {
+	for key, value := range dictionary {
+		if keyValueSelector(key, value) {
+			return true
+		}
+	}
+	return false
+}
+
+func All[Key comparable, Value any](dictionary map[Key]Value, keyValueSelector func(key Key, value Value) bool) bool {
+	for key, value := range dictionary {
+		if !keyValueSelector(key, value) {
+			return false
+		}
+	}
+	return true
+}

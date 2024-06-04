866feb96b61395519fa052edb9f395e75e21df72
Marcin Ozga <nuklearny45@poczta.onet.pl>
2024-06-04 22:26:34 +0200

    Sum, min ,max

--- Where.go
+++ Where.go
@@ -1,5 +1,68 @@
 package gomaps
 
+import (
+	"errors"
+
+	"golang.org/x/exp/constraints"
+)
+
+var ErrSequenceContainsNoElements error = errors.New("sequence contains no elements")
+
+func Max[Key comparable, Value any, Max constraints.Ordered](dictionary map[Key]Value, maxSelector func(key Key, value Value) Max) (max Max, err error) {
+	if len(dictionary) == 0 {
+		return max, ErrSequenceContainsNoElements
+	}
+
+	for key, value := range dictionary {
+		max = maxSelector(key, value)
+		break
+	}
+
+	if len(dictionary) == 1 {
+		return max, nil
+	}
+
+	for key, value := range dictionary {
+		value := maxSelector(key, value)
+		if value > max {
+			max = value
+		}
+	}
+
+	return max, nil
+}
+
+func Min[Key comparable, Value any, Min constraints.Ordered](dictionary map[Key]Value, maxSelector func(key Key, value Value) Min) (min Min, err error) {
+	if len(dictionary) == 0 {
+		return min, ErrSequenceContainsNoElements
+	}
+
+	for key, value := range dictionary {
+		min = maxSelector(key, value)
+		break
+	}
+
+	if len(dictionary) == 1 {
+		return min, nil
+	}
+
+	for key, value := range dictionary {
+		value := maxSelector(key, value)
+		if value < min {
+			min = value
+		}
+	}
+
+	return min, nil
+}
+
+func Sum[Key comparable, Value any, ReturnValue constraints.Integer | constraints.Float | constraints.Complex](dictionary map[Key]Value, valueSelector func(key Key, value Value) ReturnValue) (sum ReturnValue) {
+	for key, value := range dictionary {
+		sum += valueSelector(key, value)
+	}
+	return sum
+}
+
 func Where[Dictionary map[Key]Value, Key comparable, Value any](dictionary Dictionary, keyValueSelector func(key Key, value Value) bool) Dictionary {
 	m := make(Dictionary)
 	for key, value := range dictionary {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/marcin-ozga/gomaps
 
 go 1.21.0
+
+require golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
